bfec20a4fed0dbe13c1af4e52d02677eb58d76be
William Nilsson <william.nilsson.wn@gmail.com>
2024-01-16 15:59:15 +0000

    Actually adding bugfix

--- Performance/MultiPath/sss_receiver.py
+++ Performance/MultiPath/sss_receiver.py
@@ -169,7 +169,7 @@ def test_rsa(iters):
             s.bind((HOST, PORT))
             s.listen()
             conn, addr = s.accept()
-            key = pickle.loads(conn.recv(4096))      
+            key = pickle.loads(conn.recv(8192))      
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((HOST, PORT))
